7e1dd9a1aec9c6237d8893d1daf12dd208ae0129
Lev <leva.averin@yandex.ru>
2023-06-27 19:43:41 +0300

    Added base Kafka event and EventSubmitted

---
+++ src/main/java/com/eventmanagement/shared/kafkaEvents/KafkaEvent.java
@@ -0,0 +1,15 @@
+package com.eventmanagement.shared.kafkaEvents;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.Instant;
+
+@Getter
+@Setter
+public abstract class KafkaEvent {
+    protected Instant timestamp;
+    public abstract String getEventType();
+    public abstract String getTopic();
+
+}
---
+++ src/main/java/com/eventmanagement/shared/kafkaEvents/event/EventSubmitted.java
@@ -0,0 +1,26 @@
+package com.eventmanagement.shared.kafkaEvents.event;
+
+import com.eventmanagement.shared.dto.request.EventDTO;
+import com.eventmanagement.shared.kafkaEvents.KafkaEvent;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.Instant;
+
+@Getter
+@Setter
+public class EventSubmitted extends KafkaEvent {
+    public EventSubmitted(EventDTO eventDTO) {
+        this.timestamp = Instant.now();
+        this.event = eventDTO;
+    }
+    @Override
+    public String getEventType() {
+        return "EventSubmitted";
+    }
+    @Override
+    public String getTopic() {
+        return "event-submitted-kafka-events";
+    }
+    private EventDTO event;
+}
