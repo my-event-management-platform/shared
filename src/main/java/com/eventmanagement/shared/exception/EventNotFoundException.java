f12a34c6f8c7679af7c13ff6691a48f0bdc7fae5
Lev <leva.averin@yandex.ru>
2023-06-28 18:51:21 +0300

    Deleted EventNotFoundException (moved to respective service)

--- src/main/java/com/eventmanagement/shared/exception/EventNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.eventmanagement.shared.exception;
-
-public class EventNotFoundException extends RuntimeException{
-    public EventNotFoundException(String message) {
-        super(message);
-    }
-}
