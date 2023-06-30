2dfc44b5cac4e8b96f3d09b2e052061f41aff033
Lev <leva.averin@yandex.ru>
2023-06-30 19:22:56 +0300

    Added EventChanged

---
+++ src/main/java/com/eventmanagement/shared/kafkaEvents/event/EventChanged.java
@@ -0,0 +1,22 @@
+package com.eventmanagement.shared.kafkaEvents.event;
+
+import com.eventmanagement.shared.kafkaEvents.KafkaEvent;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.Instant;
+
+@Getter
+@Setter
+public class EventChanged extends KafkaEvent {
+    public String getTopic() {
+        return "event-changed-kafka-events";
+    }
+    private String id;
+    private String title;
+    private String text;
+    private String location;
+    private Instant datetime;
+    private Integer capacity;
+    private String userId;
+}
