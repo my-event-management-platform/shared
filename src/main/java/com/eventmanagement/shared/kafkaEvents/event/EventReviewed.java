632d9e161bb79dd706534196b47ac7a1925b9b5d
Lev <leva.averin@yandex.ru>
2023-07-05 21:29:43 +0300

    Added no args constructor for EventReviewed

--- src/main/java/com/eventmanagement/shared/kafkaEvents/event/EventReviewed.java
+++ src/main/java/com/eventmanagement/shared/kafkaEvents/event/EventReviewed.java
@@ -2,20 +2,18 @@ package com.eventmanagement.shared.kafkaEvents.event;
 
 import com.eventmanagement.shared.kafkaEvents.KafkaEvent;
 import com.eventmanagement.shared.types.ReviewDecision;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.RequiredArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import java.time.Instant;
 
 @Getter
 @Setter
 @AllArgsConstructor
+@NoArgsConstructor
 public class EventReviewed extends KafkaEvent {
     @Override
     public String getTopic() {
-        return "event-submitted-kafka-events";
+        return "event-reviewed-kafka-events";
     }
     private String eventId;
     private ReviewDecision reviewDecision;
