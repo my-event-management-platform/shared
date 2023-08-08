a50f7c4c096d71e967ff6c246946fd454c47f1b6
Lev <leva.averin@yandex.ru>
2023-08-08 12:15:40 +0300

    Added new endpoint with information about Event's bookings

---
+++ src/main/java/com/eventmanagement/shared/dto/response/EventBookingsInfoDTO.java
@@ -0,0 +1,11 @@
+package com.eventmanagement.shared.dto.response;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class EventBookingsInfoDTO {
+    private Integer initialCapacity;
+    private Integer capacityLeft;
+}
